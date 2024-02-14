cd5ba4cda89bee8411ff168eaa795bc4c00818b7
Amir Adilov <amiradilov202@gmail.com>
2024-02-14 22:30:49 +0600

    Update style and functionality of to-do list

--- Lab3/to-do-list/static/js/list.js
+++ Lab3/to-do-list/static/js/list.js
@@ -1,14 +1,22 @@
 function addItem() {
     const newItemInput = document.getElementById("newItemInput");
     const todoList = document.getElementById("todoList");
-
     const newItem = document.createElement("li");
     newItem.innerHTML = `
         <input type="checkbox" onchange="markAsDone(this)">
         <span>${newItemInput.value}</span>
-        <button class="delete" onclick="deleteItem(this)"><img src="https://www.svgrepo.com/show/21045/delete-button.svg" width=10px></button>
-    `;
+        <button class="delete" onclick="deleteItem(this)">
+        <img src="https://www.svgrepo.com/show/21045/delete-button.svg" width=10px>
+        </button>
 
+        <button class="rewrite" onclick="rewriteItem(this)">
+        <img src="https://illustoon.com/photo/3124.png" width=10px>
+        </button>
+    `;
+    if(newItemInput.value === "") {
+        alert("Please enter a task")
+        return;
+    }
     todoList.appendChild(newItem);
     newItemInput.value = "";
 }
@@ -23,6 +31,19 @@ function markAsDone(checkbox) {
 }
 
 function deleteItem(button) {
+    if(confirm("Are you sure you want to delete this task?") === false) {
+        return;
+    }
     const listItem = button.parentNode;
     listItem.remove();
+}
+
+function rewriteItem(button) {
+    const listItem = button.parentNode;
+    const itemText = listItem.querySelector("span");
+    const newText = prompt("Edit task", itemText.innerText);
+    if(newText === null) {
+        return;
+    }
+    itemText.innerText = newText;
 }
