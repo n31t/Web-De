b52a1e6652770714d923ae9e39e7ebe99a2c2732
Amir Adilov <amiradilov202@gmail.com>
2024-03-13 22:20:38 +0600

    Update Angular configuration and fix routing

--- Lab6/src/app/app.routes.ts
+++ Lab6/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { AboutComponent } from './about/about.component';
 import { AlbumsComponent } from './albums/albums.component';
 import { AlbumDetailComponent } from './album-detail/album-detail.component';
 import { AlbumPhotosComponent } from './album-photos/album-photos.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
-    {path: 'home', component: HomeComponent},
-    {path: 'about', component: AboutComponent},
-    {path: 'albums', component: AlbumsComponent},
-    {path: 'albums/:id', component: AlbumDetailComponent},
-    {path: 'albums/:id/photos', component: AlbumPhotosComponent}
+    {path: 'home', component: HomeComponent, title: 'Home'},
+    {path: 'about', component: AboutComponent, title: 'About'},
+    {path: 'albums', component: AlbumsComponent, title: 'Albums'},
+    {path: 'albums/:id', component: AlbumDetailComponent, title: 'Album Detail'},
+    {path: 'albums/:id/photos', component: AlbumPhotosComponent, title: 'Album Photos'},
+    {path: '**', component: NotFoundComponent, title : 'Not Found'}
 ];
